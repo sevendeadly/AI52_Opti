5ab2e2704eefbb3727bdab8659026d39c40cacbc
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-01-09 17:52:16 +0100

    Release: beta version of Ant Colony Optimization

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,3 +1,8 @@
+# This file contains all the constants used in the program
+
+# librairies import
+from src.utils.time import DAYTIME
+
 # all the constraints definitions to run the program (time in minutes)
 LOCOMOTION_CAPACITY = 50
 SERVICE_START = 6 * 60
@@ -7,7 +12,9 @@ MAX_TIME_BETWEEN_STOPS = 10
 NUM_LOCOMOTIONS = 100
 MIN_DEMANDS = 1800
 MAX_DEMANDS = 2100
+NUM_PROGS = 100
 DIRECTION_REPARTITION = (50, 50)
+PEAK_REPARTITION = [(DAYTIME.MORNING, 35), (DAYTIME.DAY, 20), (DAYTIME.EVENING, 35), (DAYTIME.NIGHT, 10)]
 PEAK_TIME_INTERVALS = [(SERVICE_START, 10 * 60), (10 * 60, 16 * 60), (16 * 60, 20 * 60), (20 * 60, SERVICE_END)]
 MAX_LOCOMOTION_SLOT_VARIATION = 10
 DEMAND_INSTANCE_HEADERS = ['Arrival time', 'Boarding stop', 'Stops to go', 'Direction']
